b073d85dbab160b89b264efce2a290bfaecdec95
= <trongvang.uncun@gmail.com>
2020-07-18 09:33:57 +0700

    update code 2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -154,6 +154,204 @@
             </div>
             
         </section>
+        <section id="our-case-studies">
+            <div class="container content-ocs">
+                <h1>our case <span>studies</span></h1>
+                <div class="desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                </div>
+                <div class="options">
+                    <a href="#">all</a>
+                    <a href="#">active but</a>
+                    <a href="#">hover but</a>
+                    <a href="#">category 3</a>
+                </div>
+                <div class="square">
+                    <div class="row-1" style="display:flex">
+                        <div style="background-color:red; flex:50%; ">aaaa</div>
+                        <div style="background-color:green; flex:50%; display:flex; flex-wrap:wrap">
+                            <div style="background-color:blue; flex:50%; ">a</div>
+                            <div style="background-color:black; flex:50%; ">a</div>
+                            <div style="background-color:yellow; flex:50%; ">a</div>
+                            <div style="background-color:green; flex:50%; ">a</div>
+                        </div>
+                    </div>
+                    <div class="row-2" style="display:flex">
+                        <div style="background-color:blue; flex:1;">a</div>
+                        <div style="background-color:yellow; flex:1;">a</div>
+                        <div style="background-color:red; flex:2;">a</div>
+                    </div>
+                </div>
+                <div class="more-work">
+                    <div class="row">
+                    </div>
+                    <div class="button">
+                        <button>more work</button>
+                    </div>
+                    <div class="row">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="core-competence">
+            <div class="container core-head">
+                <h1>core <span>competence</span></h1>
+                <div class="desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                </div>
+            </div>
+            <div class="core-content">
+                <div class="bg-head">
+                    <div class="box-left"></div>
+                    <div class="box-right"></div>
+                </div>
+                <div class="bg-items">
+                    <div class="container items">
+                        <div class="item">
+
+                            <img src="../assets/imgs/core-icon.png" alt="" height="125px">
+                            <h2>consulting</h2>
+                            <div class="desc">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            </div>
+                            <div class="button">
+                                <button>learn more</button>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="../assets/imgs/core-icon.png" alt="" height="125px">
+                            <h2>consulting</h2>
+                            <div class="desc">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            </div>
+                            <div class="button">
+                                <button>learn more</button>
+                            </div>
+                        </div>
+                        <div class="item ">
+                            <img src="../assets/imgs/core-icon.png" alt="" height="125px">
+                            <h2>consulting</h2>
+                            <div class="desc">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            </div>
+                            <div class="button">
+                                <button>learn more</button>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="../assets/imgs/core-icon.png" alt="" height="125px">
+                            <h2>consulting</h2>
+                            <div class="desc">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            </div>
+                            <div class="button">
+                                <button>learn more</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="testtimonials">
+            <div class="container content-test">
+                <img src="../assets/imgs/counters-icon.png" alt="">
+                <div class="name">Jeniffer Burns</div>
+                <div class="short-desc">Creative Heads Inc.</div>
+                <div class="long-desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                </div>
+                <div class="icon">"</div>
+            </div>
+        </section>
+        <section id="pricing">
+            <div class="container content-pri">
+                <h1 class="title">pricing <span>table</span></h1>
+                <div class="desc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+                </div>
+                <div class="items">
+                    <div class="item">
+                        <div class="item-title"><div>
+                                master
+                            </div></div>
+                        <div class="item-price">
+                            <div class="price">$10</div>
+                            <div>per month</div>
+                        </div>
+                        <ul>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through none-border">Photo sharing school</li>
+                        </ul>
+                        <button>order</button>
+                    </div>
+                    <div class="item">
+                        <div class="item-title"><div>
+                                master
+                            </div></div>
+                        <div class="item-price">
+                            <div class="price">$10</div>
+                            <div>per month</div>
+                        </div>
+                        <ul>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through none-border">Photo sharing school</li>
+                        </ul>
+                        <button>order</button>
+                    </div>
+                    <div class="item">
+                        <div class="new">new</div>
+                        <div class="item-title">
+                            <div>
+                                master  
+                            </div>
+                            <p>The best price/quantity plan</p>
+                        </div>
+                        <div class="item-price">
+                            <div class="price">$10</div>
+                            <div>per month</div>
+                        </div>
+                        <ul>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through none-border">Photo sharing school</li>
+                        </ul>
+                        <button>order</button>
+                    </div>
+                    <div class="item">
+                        <div class="item-title"><div>
+                                master
+                            </div></div>
+                        <div class="item-price">
+                            <div class="price">$10</div>
+                            <div>per month</div>
+                        </div>
+                        <ul>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li>Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through">Photo sharing school</li>
+                            <li class="line-through none-border">Photo sharing school</li>
+                        </ul>
+                        <button>order</button>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
    
 </template>
